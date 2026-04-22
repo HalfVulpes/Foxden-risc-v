6097023423c42472c3542629b1ddfa1dd1d18c7b
HalfVulpes <rex@mail.leafwolf.net>
2026-04-22 04:52:55 -0400

    remove garbage

--- workspace/Foxden_IO_RV64GC_1/system-rk-xcku5p/firrtl_black_box_resource_files.f
+++
@@ -1,1 +0,0 @@
-/home/rex/Desktop/Foxden-risc-v/workspace/Foxden_IO_RV64GC_1/system-rk-xcku5p/plusarg_reader.v
--- workspace/Foxden_IO_RV64GC_4_L2W/system-rk-xcku5p/firrtl_black_box_resource_files.f
+++
@@ -1,1 +0,0 @@
-/home/rex/Desktop/Foxden-risc-v/workspace/Foxden_IO_RV64GC_4_L2W/system-rk-xcku5p/plusarg_reader.v
